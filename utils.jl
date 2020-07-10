6811fd1e20dce7bd1744039cd683675b7e76ed3b
PyDataBlog <pimpfada@gmail.com>
2020-07-10 23:15:26 +0200

    Initialised util script

---
+++ utils.jl
@@ -0,0 +1,97 @@
+"""
+"""
+function sigmoid()
+
+end
+
+
+"""
+"""
+function sigmoid_backwards()
+
+end
+
+
+"""
+"""
+function relu()
+
+end
+
+
+"""
+"""
+function relu_backwards()
+
+end
+
+
+"""
+"""
+function linear_forward()
+
+end
+
+
+"""
+"""
+function linear_backward()
+
+end
+
+
+"""
+"""
+function linear_backward_activation()
+
+end
+
+
+"""
+"""
+function linear_forward_activation()
+
+end
+
+
+"""
+"""
+function initialise_model_weights()
+
+end
+
+
+"""
+"""
+function forward_propagate_model_weights()
+
+end
+
+
+"""
+"""
+function calculate_cost()
+
+end
+
+
+"""
+"""
+function back_propagate_model_weights()
+
+end
+
+
+"""
+"""
+function update_model_weights()
+
+end
+
+
+"""
+"""
+function predict()
+
+end
+
